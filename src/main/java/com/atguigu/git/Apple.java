572d2823bacaab10023f4e120058e2a76fb2b784
545877604 <545877604@qq.com>
2019-01-21 19:27:20 +0800

    edit on branch hot_fix

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -4,7 +4,8 @@ public class Apple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("left...");
+		System.out.println("left ...");
+		System.out.println("hot fix ...");
 
 	}
 
