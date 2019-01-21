93466d6ee419c314f3fd5b6334eab1b182338e7f
545877604 <545877604@qq.com>
2019-01-21 19:17:02 +0800

    改成right

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -4,7 +4,7 @@ public class Apple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("right...");
+		System.out.println("right...。。");
 
 	}
 
