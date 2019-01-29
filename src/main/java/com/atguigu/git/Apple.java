d51fd89c3e0e72a345373e494989643aaf47209d
Administrator <Administrator>
2019-01-29 21:12:10 +0800

    master

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -3,17 +3,9 @@ package com.atguigu.git;
 public class Apple {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("left...");
-		System.out.println("hot fix ...");
-		System.out.println("right...。。");
-
+	
 		System.out.println("master...。。");
-		System.out.println("hand...。。");
-
-		System.out.println("branch-bug...。。");
-		System.out.println("branch-bug 2...。。");
-		System.out.println("branch-bug 3...。。");
+	
 
 	}
 
